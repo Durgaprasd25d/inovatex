187f4d677ad1e655bba9a2ae2f3b2867108af135
Durgaprasad Dalai <durgaprasaddalai10@gmail.com>
2025-04-01 13:41:36 +0530

    footer:Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
               <div className="flex items-center text-gray-400">
                 <Phone className="w-5 h-5 mr-2" />
                 <a href="tel:+918895555987" className="hover:text-blue-500 transition-colors">
-                  +91 8895555987
+                  +91 123456789
                 </a>
               </div>
             </div>
